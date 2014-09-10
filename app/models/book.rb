8806857d4f94d0779179a86b8e4ccf83ca7b9bbe
amisukanta <amisukanta02@gmail.com>
2014-09-10 02:26:41 +0200

    Initial search title only

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,17 @@ class Book < ActiveRecord::Base
 
 	scope :finished, ->{where.not(finished_on: nil)}
 	scope :recent, ->{where('finished_on > ?', 2.days.ago)} 
-	
+	# SCOPE IS GOING TO RETURN A COLLECTION, 
+	# IF KEYWORD IS NOT PRESENT, ITS NOT GOING TO RETURN NIL 
+	# RATHER GOING TO RETURN ALL 
+	scope :search, ->(keyword){where(title: keyword) if keyword.present? }
+	#def self.search(keyword)
+	# if keyword.present?
+	#	where(title: keyword)
+	# else
+	#  	all
+	#  end
+	#end
 
 	def finished?
 	  finished_on.present?

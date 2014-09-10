c5ac38f081db5f610c50b91cdf5b97225574f687
amisukanta <amisukanta02@gmail.com>
2014-09-10 02:45:40 +0200

    improved search with keywords

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,7 +5,16 @@ class Book < ActiveRecord::Base
 	# SCOPE IS GOING TO RETURN A COLLECTION, 
 	# IF KEYWORD IS NOT PRESENT, ITS NOT GOING TO RETURN NIL 
 	# RATHER GOING TO RETURN ALL 
-	scope :search, ->(keyword){where(title: keyword) if keyword.present? }
+	scope :search, ->(keyword){where('keywords LIKE ?', "%#{keyword.downcase}%") if keyword.present? }
+	
+	before_save :set_keywords
+
+
+
+	protected
+	  def set_keywords
+	  	self.keywords = [title, author, description].map(&:downcase).join(' ')
+	  end
 	#def self.search(keyword)
 	# if keyword.present?
 	#	where(title: keyword)
@@ -13,6 +22,8 @@ class Book < ActiveRecord::Base
 	#  	all
 	#  end
 	#end
+
+
 
 	def finished?
 	  finished_on.present?
